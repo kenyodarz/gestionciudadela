8ae1ccb15066c3b9fdda26be74ee99adfbd57702
Jorge Mina <minajorge@hotmail.com>
2021-05-19 11:18:45 -0500

    Initial commit

---
+++ src/main/java/com/bykenyodarz/gestionciudadela/GestionciudadelaApplication.java
@@ -0,0 +1,13 @@
+package com.bykenyodarz.gestionciudadela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionciudadelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionciudadelaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bykenyodarz/gestionciudadela/GestionciudadelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bykenyodarz.gestionciudadela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionciudadelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
