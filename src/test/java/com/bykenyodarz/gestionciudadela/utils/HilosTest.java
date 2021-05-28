edfa42a893b97dbb6a5985374d45e54fb265009c
Jorge Mina <minajorge@hotmail.com>
2021-05-28 09:12:35 -0500

    Terminado los ajustes de Docker

---
+++ src/test/java/com/bykenyodarz/gestionciudadela/utils/HilosTest.java
@@ -0,0 +1,12 @@
+package com.bykenyodarz.gestionciudadela.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HilosTest {
+
+    @Test
+    void startScheduleAtFixedRateTask() {
+    }
+}
